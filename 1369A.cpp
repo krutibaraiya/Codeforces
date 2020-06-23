68b3a6f6a072a7d0d5e6ae0be353f2b44fdc4f7d
Kruti Baraiya <57318920+krutibaraiya@users.noreply.github.com>
2020-06-23 22:24:57 +0530

    FashionabLee

---
+++ 1369A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long
+const int N = 1e5 + 5;
+ 
+
+
+
+int32_t main()
+{
+   IOS;
+   int t;
+   cin >> t;
+
+   while(t--)
+   {
+   		int n;
+   		cin >> n;
+
+   		if(n%4==0)
+   			cout << "YES" << endl;
+   		else 
+   			cout << "NO" << endl;
+   }
+
+
+   return 0;
+}
+ 
